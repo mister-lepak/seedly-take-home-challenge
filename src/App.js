a129808a2b4ad67fe6aa5f531168bcc75114f3c9
Try Wijono <3wijono@gmail.com>
2021-01-29 22:04:43 +0800

    initialize components and import to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Sidebar from "./components/Sidebar";
+import QnA from "./components/QnA";
+
 function App() {
   return (
-    <div>
-      <header></header>
-    </div>
+    <>
+      <Navbar />
+      <Banner />
+      <Sidebar />
+      <QnA />
+    </>
   );
 }
 
---
+++ src/components/QnA.js
@@ -0,0 +1,3 @@
+const QnA = () => {};
+
+export default QnA;
